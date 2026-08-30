4a49067981910d8527ef44fe32b2196f1b8aa3dd
nys1998 <ngyisheng0425@gmail.com>
2026-08-30 21:31:03 +0800

    Done around_point

--- Cubeic.py
+++ Cubeic.py
@@ -1,7 +1,8 @@
 #Cube file read and process
 import numpy as np
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import LinearNDInterpolator, NearestNDInterpolator
 from itertools import combinations
+from scipy.spatial import KDTree
 
 class CubeFile:
     """
@@ -78,9 +79,13 @@ class ProcessGrid():
         self.cube = cube.data
         self.axis = np.array(cube.axis)
         self.n = cube.n
+        self.axis_scaled = self.axis * self.n
         self.frac_coord = cube.get_coordinates(fractional=True)
         self.max_frac = np.max(self.frac_coord[:,:-1],axis=0)
         self.interpolator = None
+        
+        # Pre-compute absolute grid positions
+        self.grid = self.data[:, :-1]
 
     def Nearest_Coord(self,coord):
         subtract = []
@@ -98,21 +103,23 @@ class ProcessGrid():
         if len(coord.shape) == 1 or coord.shape[0] == 1:
             return coord[0]*axes[0] + coord[1]*axes[1] + coord[2] * axes[2]
         else:
-            return coord[:,0]*axes[0] + coord[:,1]*axes[1] + coord[:,2]*axes[2]
+            return np.outer(coord[:, 0], axes[0]) + np.outer(coord[:, 1], axes[1]) + np.outer(coord[:, 2], axes[2])
 
     @staticmethod
     def abs_to_frac(coord,axes):
         coord = np.array(coord)
         return (np.linalg.inv(axes) @ coord.T).T
-        # if len(coord.shape) == 1 or coord.shape[0] == 1:
-        #     return np.matmul(np.linalg.inv(axes),coord)
-        # if len(coord.shape) == 1 or coord.shape[0] == 1:
-        #     return 
 
-    # def map_to_cell(self,coord):
-    #     wrap = lambda x, tol=1e-12: 0.0 if abs(x % 1.0) < tol or abs(x % 1.0 - 1.0) < tol else x % 1.0
-    #     coord = self.abs_to_frac(coord)
-    #     return self.frac_to_abs([int(wrap(coord[0])*self.n[0]), int(wrap(coord[1])*self.n[1]),int(wrap(coord[2])*self.n[2])])
+    def map_to_cell(self,coord):
+        wrap = lambda x, tol=1e-12: np.where((np.abs(x % 1.0) < tol) | (np.abs(x % 1.0 - 1.0) < tol), 0.0,
+        x % 1.0)
+        coord_wrapped = wrap(self.abs_to_frac(coord,self.axis_scaled))
+        coord_wrapped = self.frac_to_abs(coord_wrapped,self.axis_scaled)
+        dif = coord_wrapped - coord
+        _, indices = self.kdtree.query(coord_wrapped)
+        closest = self.data[indices] 
+        closest[:,:-1] = closest[:,:-1] - dif
+        return closest
 
     @staticmethod
     def one_arr(i):
@@ -155,67 +162,41 @@ class ProcessGrid():
                     for j in range(dist[1]):
                         for k in range(dist[2]):
                             # Coordinate of the voxel corner
+                            
                             coord = origin_frac + np.array([i/(self.n[0]),j/(self.n[1]),k/(self.n[2])])
                             weight = self.cube[int(coord[0]%1)*self.n[0], int(coord[1] %1)*self.n[1], int(coord[2]%1)*self.n[2] ] # type: ignore
                             new_grid.append([coord[0], coord[1], coord[2], weight])
         return new_grid
     
 
-    def around_point(self,coord,radius=1):
-        # self.interpolator = LinearNDInterpolator(self.cube.get_coordinate(), values)
-        # coord_frac = self.abs_to_frac(coord,self.axis*self.n[0])
-        data = self.data[np.where(np.linalg.norm(self.data[:,:-1]-coord,axis=1) < radius)]
+    def around_point(self,coord,length=1):
+        # Pre-compute inverse matrix once
+        self.inv_axes = np.linalg.inv(self.axis_scaled)
+        
+        # Pre-compute wrapped grid points in fractional coordinates
+        # This avoids repeated wrapping of the grid
+        self.grid_frac = self.abs_to_frac(self.grid,self.axis_scaled)
+        
+        # Build KD-tree for fast nearest neighbor search
+        self.kdtree = KDTree(self.grid)
 
-        vertices = []
-        for i in range(3):
-            for j in [-1,1]:
-                    if coord[i] + j < 0 or coord[i] + j > 1:
-                        vertices.append(j)
-                        temp = self.data[np.where(np.linalg.norm(self.data[:,:-1]-coord+j*self.one_arr(i)
-                                                                 ,axis=1) < radius)]      
-                        temp[:,:-1] = temp[:,:-1]+j*self.one_arr(i)
-                        data = np.vstack((data,temp))
-                    else:
-                        vertices.append(0)
-        for i in range(3):
-            for j in [-1,1]:
-                    for m in range(3):
-                        if i == m:
-                            continue
-                        for k in [-1,1]:
-                            # if coord[i] + j < 0 or coord[i] + j > 1:
-                            temp = self.data[np.where(np.linalg.norm(self.data[:,:-1]-coord+j*self.one_arr(i)
-                                                                +k*self.one_arr(m),axis=1) < radius)]      
-                            temp[:,:-1] = temp[:,:-1]+j*self.one_arr(i) +k*self.one_arr(m)
-                            data = np.vstack((data,temp))
-                        for p in range(3):
-                            if i == p  or m == p:
-                                 continue
-                            for q in [-1,1]:
-                                    # if coord[i] + j < 0 or coord[i] + j > 1:
-                                    temp = self.data[np.where(np.linalg.norm(self.data[:,:-1]-coord+j*self.one_arr(i)
-                                                            +k*self.one_arr(m)+q*self.one_arr(p),axis=1) < radius)]      
-                                    temp[:,:-1] = temp[:,:-1]+j*self.one_arr(i) +k*self.one_arr(m)+q*self.one_arr(p)
-                                    data = np.vstack((data,temp))    
-        vertices=np.array(vertices)
-        if len(np.nonzero(vertices)) == 0:
-            return data
-        return data
+        npt = 30 
+        while True:
+            x = np.linspace(coord[0]-length/2,coord[0]+length/2, npt)
+            y = np.linspace(coord[1]-length/2,coord[1]+length/2, npt)
+            z = np.linspace(coord[2]-length/2,coord[2]+length/2, npt)
+            # Create meshgrid
+            X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
 
-        ind = np.where(vertices != 0)
-        if len(ind) >= 2:  
-            for k in combinations(ind,r=2):
-                temp = self.data[np.where(np.linalg.norm(self.data[:,:-1]-coord+vertices[k[0]]*self.one_arr(np.ceil(k[0]/2)) 
-                                                    + vertices[k[1]]*self.one_arr(np.ceil(k[1]/2)),axis=1) < radius)]      
-                temp[:,:-1] = temp[:,:-1]+j*self.one_arr(i)
-                data = np.vstack((data,temp))
-
-            if len(ind) == 3:
-                temp = self.data[np.where(np.linalg.norm(self.data[:,:-1]-coord+vertices[ind[0]]*self.one_arr(0) 
-                                    + vertices[ind[1]]*self.one_arr(1)+ vertices[ind[2]]*self.one_arr(2),axis=1) < radius)]      
-                temp[:,:-1] = temp[:,:-1]+j*self.one_arr(i)
-                data = np.vstack((data,temp))
-        return data
+            # Combine into array of [x, y, z] points
+            combined = np.stack([X, Y, Z], axis=-1).reshape(-1, 3)
+            mapped = self.map_to_cell(combined)
+            if mapped.shape != np.unique(mapped,axis=0).shape:
+                npt -= 1
+                print(npt)
+            else:
+                print(npt,'OK')
+                return mapped
         
 c = CubeFile("defc_afm1_up.cube")
 grid = ProcessGrid(c)
